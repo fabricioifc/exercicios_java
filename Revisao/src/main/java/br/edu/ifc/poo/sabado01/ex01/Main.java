276388a36dad27479e71e91f9a0f81f189711bfd
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2019-03-09 09:39:27 -0300

    Exercícios resolvidos

---
+++ Revisao/src/main/java/br/edu/ifc/poo/sabado01/ex01/Main.java
@@ -0,0 +1,21 @@
+package br.edu.ifc.poo.sabado01.ex01;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+  public static void main(String[] args) {
+    try {
+      Semana teste = new Semana();
+
+      String entrada = JOptionPane.showInputDialog("Informe o dia: ");
+      int dia = Integer.parseInt(entrada);
+
+      String resposta = teste.verificarDia(dia);
+      JOptionPane.showMessageDialog(null, resposta);
+    } catch (NumberFormatException erro) {
+      erro.printStackTrace();
+      JOptionPane.showMessageDialog(null, "Informe um número!");
+    }
+  }
+}
