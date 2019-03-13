683865ca3474a3a8d09ea35004462130f62760df
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2019-03-13 20:30:09 -0300

    Exercícios resolvidos

---
+++ Revisao/src/main/java/br/edu/ifc/poo/aula04/exercicios/ex01/Nose.java
@@ -0,0 +1,5 @@
+package br.edu.ifc.poo.aula04.exercicios.ex01;
+
+public interface Nose {
+  int iMethod();
+}
---
+++ Revisao/src/main/java/br/edu/ifc/poo/aula04/exercicios/ex01/Of76.java
@@ -0,0 +1,16 @@
+package br.edu.ifc.poo.aula04.exercicios.ex01;
+
+public class Of76 extends Clows {
+
+  public static void main(String[] args) {
+    Nose[] i = new Nose[3];
+    i[0] = new Acts();
+    i[1] = new Clows();
+    i[2] = new Of76();
+
+    for (int x = 0; x < 3; x++) {
+      System.out.println(i[x].iMethod() + " " + i[x].getClass());
+    }
+  }
+
+}
