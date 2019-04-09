41d608dd98a00e6e2c60a62c4a4bbeec457f6048
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2019-04-09 14:39:16 -0300

    prov

---
+++ AulasPII/src/main/java/br/edu/ifc/poo/aula10/prova/ObjetoGeometrico.java
@@ -0,0 +1,10 @@
+package br.edu.ifc.poo.aula10.prova;
+
+interface ObjetoGeometrico {
+
+    //@return a área do objeto
+    double calcularArea();
+
+    //@return o perímetro do objeto
+    double calcularPerimetro();
+}
